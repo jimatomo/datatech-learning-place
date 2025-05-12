02b7dfe625e1bc6c0f0c6a1486ebf7df1eaef3a4
jimatomo <iitomoamuz@gmail.com>
2025-05-12 13:49:30 +0000

    feat: BadgeListコンポーネントのロジックを修正し、beginnerバッチを追加

--- app/harvor/ui/badge_list.tsx
+++ app/harvor/ui/badge_list.tsx
@@ -5,19 +5,28 @@ interface BadgeListProps {
 }
 
 export const BadgeList = ({ correctAnswers }: BadgeListProps) => {
-  if (correctAnswers < 100) {
+  if (correctAnswers < 1) {
     return null;
   }
 
   return (
-    <div className="self-start">
+    <div className="self-start flex space-x-2">
       <Image
-        src="https://datatech-learning-place.net/logo/badge_quiz100.png"
-        alt="Quiz 100 Badge"
+        src="https://datatech-learning-place.net/logo/badge_quiz_beginner.png"
+        alt="Quiz Beginner Badge"
         width={100}
         height={100}
         className="rounded-lg shadow-md"
       />
+      {correctAnswers >= 100 && (
+        <Image
+          src="https://datatech-learning-place.net/logo/badge_quiz100.png"
+          alt="Quiz 100 Badge"
+          width={100}
+          height={100}
+          className="rounded-lg shadow-md"
+        />
+      )}
     </div>
   );
 };
